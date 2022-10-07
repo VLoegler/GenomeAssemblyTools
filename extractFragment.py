88028bd3449797b999bf7cce1502cc07d64a5093
VLoegler <52069177+VLoegler@users.noreply.github.com>
2022-10-07 14:37:32 +0200

    Update extractFragment.py

--- extractFragment.py
+++ extractFragment.py
@@ -3,14 +3,14 @@
 #----------------------------------------------------------------------------
 # Created By  : vloegler
 # Created Date: 2022/09/15
-# version ='1.1'
+# version ='1.2'
 # ---------------------------------------------------------------------------
 '''
 This script extract a sequence from a fasta file
 
 It takes as input :
 	-f --fasta: fasta file
-	-id --seqid: ID of the sequence
+	-id --seqid: ID of the sequence, or multiple IDs separated by space
 	-s --start: Start position (inclusive, 1-based)
 	-e --end: End position (inclusive, 1-based)
 	-o --output: output file
@@ -30,7 +30,7 @@ import re
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--fasta", help="fasta file", required=True)
 parser.add_argument("-o", "--output", help="output file", default = "")
-parser.add_argument("-id", "--seqid", help="ID of the sequence", required=True)
+parser.add_argument("-id", "--seqid", help="ID of the sequence", nargs='+', required=True)
 parser.add_argument("-s", "--start", help="Start position (inclusive, 1-based)", type = int, default = 0)
 parser.add_argument("-e", "--end", help="End position (inclusive, 1-based)", type = int, default = 0)
 
@@ -39,7 +39,7 @@ args = parser.parse_args()
 
 fastaPath = args.fasta
 outputPath=args.output
-seqid=args.seqid
+seqidList=args.seqid
 start=int(args.start)
 end=int(args.end)
 
@@ -63,34 +63,40 @@ for line in fasta.readlines():
 Seq += [seq]
 fasta.close()
 
-
-if seqid not in Chr:
-	raise ValueError("Sequence ID is not in fasta file")
-
-targetSeq = Seq[Chr.index(seqid)]
-
-if start != 0 or end != 0:
-	if start < 1 or start > len(targetSeq) or end < 1 or end > len(targetSeq) or end <= start:
-		raise ValueError("Bad coordinates. Start and End must be 1-based, not higher than sequence length and starts < end.")
-
-if start != 0 or end != 0:
-	extractedSeq = targetSeq[(start-1):end]
-else:
-	extractedSeq = targetSeq
-
+# Open output file if necessary
 if outputPath != "":
 	out = open(outputPath, "w")
+
+for seqid in seqidList:
+	if seqid not in Chr:
+		raise ValueError("Sequence ID is not in fasta file")
+
+	targetSeq = Seq[Chr.index(seqid)]
+
 	if start != 0 or end != 0:
-		out.write(">" + seqid + "_" + str(start) + ".." + str(end) + "\n")
+		if start < 1 or start > len(targetSeq) or end < 1 or end > len(targetSeq) or end <= start:
+			raise ValueError("Bad coordinates. Start and End must be 1-based, not higher than sequence length and starts < end.")
+
+	if start != 0 or end != 0:
+		extractedSeq = targetSeq[(start-1):end]
 	else:
-		out.write(">" + seqid + "\n")
-	seq=re.sub("(.{80})", "\\1\n", extractedSeq, 0, re.DOTALL)
-	out.write(seq+"\n")
+		extractedSeq = targetSeq
+
+	if outputPath != "":
+		if start != 0 or end != 0:
+			out.write(">" + seqid + "_" + str(start) + ".." + str(end) + "\n")
+		else:
+			out.write(">" + seqid + "\n")
+		seq=re.sub("(.{80})", "\\1\n", extractedSeq, 0, re.DOTALL)
+		out.write(seq+"\n")
+	else:
+		if start != 0 or end != 0:
+			print(">" + seqid + "_" + str(start) + ".." + str(end))
+		else:
+			print(">" + seqid)
+		seq=re.sub("(.{80})", "\\1\n", extractedSeq, 0, re.DOTALL)
+		print(seq)
+
+
+if outputPath != "":
 	out.close()
-else:
-	if start != 0 or end != 0:
-		print(">" + seqid + "_" + str(start) + ".." + str(end))
-	else:
-		print(">" + seqid)
-	seq=re.sub("(.{80})", "\\1\n", extractedSeq, 0, re.DOTALL)
-	print(seq)
